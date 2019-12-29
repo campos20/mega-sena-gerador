a0acb6e6e0da1cf6a1fca036129fbf3ee7c61b30
Alexandre Campos <camposalexandreh@gmail.com>
2019-12-29 09:13:02 -0300

    Rever base url, add live demo to read me

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Abra [http://localhost:3000](http://localhost:3000) para visualizar.
 
 - React
 - React router
+
+## Live
+
+Abra [https://campos20.github.io/mega-sena-gerador](https://campos20.github.io/mega-sena-gerador) para um demo.
--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React from "react";
 import Home from "./components/Home";
 import Sobre from "./components/Sobre";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  // Prevents loss of base url while navigating.
-  const baseUrl = window.location.origin.toString();
-  console.log(window.location);
-
   return (
     <Router>
       <div>
@@ -16,14 +12,14 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a href={baseUrl} className="nav-link">
+                <Link to="/" className="nav-link">
                   Início
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a href={baseUrl + "/sobre"} className="nav-link">
+                <Link to="/sobre" className="nav-link">
                   Sobre
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
