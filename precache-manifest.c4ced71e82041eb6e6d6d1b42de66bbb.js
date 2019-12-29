85375a6f54fd9b87ec510f4663150fe3bce85844
Alexandre Campos <camposalexandreh@gmail.com>
2019-12-29 01:56:27 -0300

    Updates

---
+++ precache-manifest.c4ced71e82041eb6e6d6d1b42de66bbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cd9d7d1117c84c72ec0e523cf21ac2",
+    "url": "/mega-sena-gerador/index.html"
+  },
+  {
+    "revision": "df265475fe2f0bba332c",
+    "url": "/mega-sena-gerador/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "07e371f683efad02a1e9",
+    "url": "/mega-sena-gerador/static/css/main.df1923a9.chunk.css"
+  },
+  {
+    "revision": "df265475fe2f0bba332c",
+    "url": "/mega-sena-gerador/static/js/2.1a298ba3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mega-sena-gerador/static/js/2.1a298ba3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07e371f683efad02a1e9",
+    "url": "/mega-sena-gerador/static/js/main.91f0955d.chunk.js"
+  },
+  {
+    "revision": "acd038954b7e3f63753d",
+    "url": "/mega-sena-gerador/static/js/runtime-main.a8541fe4.js"
+  }
+]);
