7fbd69665878fc319810c8857456f2c054b139fd
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2023-12-31 12:14:31 -0300

    Move math utils and constants

--- mega-sena-gerador/src/math.functions.js
+++ mega-sena-gerador/src/math.functions.js
@@ -1,12 +1,10 @@
-var factorial = n => {
+const factorial = (n) => {
   if (n < 2) {
     return 1;
   }
   return n * factorial(n - 1);
 };
 
-var binomial = (n, p) => {
+export const binomial = (n, p) => {
   return factorial(n) / factorial(p) / factorial(n - p);
 };
-
-export default binomial;
