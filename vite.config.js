f548a15a0bac0d7e8941c9ee08508a5dd6deb5be
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2024-12-31 15:36:00 -0300

    Setup home page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mega-sena-gerador/",
   server: {
     open: true,
     port: 3000,
