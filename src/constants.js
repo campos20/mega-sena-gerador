eeba36b3afd3aa0878a9b2cfeb47f2638e67ff9a
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2025-12-29 13:21:33 -0300

    Update value to 6,00 (#21)
    * Add start script
    
    * Run npm audit fix
    
    * Update value

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const MEGA_SENA_MIN = 1;
 export const MEGA_SENA_MAX = 60;
 
 // TODO fetch from some API
-export const MEGA_SENA_VALUE = 5; // R$ 5,00
+export const MEGA_SENA_VALUE = 6; // R$ 6,00
 
 // For now, we do not allow very large values for bets, only about 100k.
 export const MAX_BET_VALUE =
